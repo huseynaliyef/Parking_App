de2c0044fc70559c1002039e0b7046e4d0835f45
huseynaliyef <huseyn.064.064@gmail.com>
2022-10-01 23:30:31 +0400

    Parking_App

---
+++ Parking App/Parking/Parking/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Parking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.son_isdifadeci = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.random_eded_qutusu = new System.Windows.Forms.TextBox();
+            this.random_cixis_ededi = new System.Windows.Forms.TextBox();
+            this.random_eded_qutusu1 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(539, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Movcud";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(539, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Bos";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(665, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "10";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(665, 73);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Giris";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cixis";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(27, 208);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(738, 186);
+            this.listBox.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(147, 12);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(169, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Parkingi Terk Eden Masinlar:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(27, 182);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(330, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Parkingde Olan Masin Nomreleri ve Daxil Olma Tarixi";
+            // 
+            // son_isdifadeci
+            // 
+            this.son_isdifadeci.Enabled = false;
+            this.son_isdifadeci.Location = new System.Drawing.Point(442, 135);
+            this.son_isdifadeci.Multiline = true;
+            this.son_isdifadeci.Name = "son_isdifadeci";
+            this.son_isdifadeci.Size = new System.Drawing.Size(210, 67);
+            this.son_isdifadeci.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::Parking.Properties.Resources.ui_text_field_clear_button_icon;
+            this.button3.Location = new System.Drawing.Point(322, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 23);
+            this.button3.TabIndex = 10;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(147, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 56);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(442, 109);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(107, 20);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.Text = "En Son Cixan Masin:";
+            // 
+            // random_eded_qutusu
+            // 
+            this.random_eded_qutusu.Location = new System.Drawing.Point(229, 135);
+            this.random_eded_qutusu.Name = "random_eded_qutusu";
+            this.random_eded_qutusu.Size = new System.Drawing.Size(100, 20);
+            this.random_eded_qutusu.TabIndex = 13;
+            // 
+            // random_cixis_ededi
+            // 
+            this.random_cixis_ededi.BackColor = System.Drawing.SystemColors.Menu;
+            this.random_cixis_ededi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.random_cixis_ededi.Enabled = false;
+            this.random_cixis_ededi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.random_cixis_ededi.ForeColor = System.Drawing.Color.Tomato;
+            this.random_cixis_ededi.Location = new System.Drawing.Point(95, 135);
+            this.random_cixis_ededi.Multiline = true;
+            this.random_cixis_ededi.Name = "random_cixis_ededi";
+            this.random_cixis_ededi.Size = new System.Drawing.Size(114, 20);
+            this.random_cixis_ededi.TabIndex = 14;
+            this.random_cixis_ededi.Text = "Random_Cixan:";
+            // 
+            // random_eded_qutusu1
+            // 
+            this.random_eded_qutusu1.Location = new System.Drawing.Point(229, 109);
+            this.random_eded_qutusu1.Name = "random_eded_qutusu1";
+            this.random_eded_qutusu1.Size = new System.Drawing.Size(100, 20);
+            this.random_eded_qutusu1.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Enabled = false;
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.ForeColor = System.Drawing.Color.Tomato;
+            this.textBox9.Location = new System.Drawing.Point(69, 109);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(140, 20);
+            this.textBox9.TabIndex = 16;
+            this.textBox9.Text = "Random_Daxilolan:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.random_eded_qutusu1);
+            this.Controls.Add(this.random_cixis_ededi);
+            this.Controls.Add(this.random_eded_qutusu);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.son_isdifadeci);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Parking App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox son_isdifadeci;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox random_eded_qutusu;
+        private System.Windows.Forms.TextBox random_cixis_ededi;
+        private System.Windows.Forms.TextBox random_eded_qutusu1;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ Parking App/Parking/Parking/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        string zaman = DateTime.Now.ToString();
+        List<int> list = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            int eded_rnd = rnd.Next(1,15);
+
+            random_eded_qutusu.Text = (eded_rnd).ToString();
+
+
+            if (textBox4.Text == "10")
+            {
+                textBox4.Text = "10";
+                
+            }
+            else
+            {
+                for (int i = listBox.Items.Count -1; i >= 0; i--)
+                {
+                    if ((listBox.Items[i].ToString()).Split(' ')[0] == eded_rnd.ToString())
+                    {
+                        son_isdifadeci.Text = "";
+                        listBox1.Items.Add(listBox.Items[i]);
+                        son_isdifadeci.Text = listBox.Items[i].ToString();
+                        for(int j = 0; j < list.Count; j++)
+                        {
+                            if (list[j] == eded_rnd)
+                            {
+                                list.RemoveAt(j);
+                            }
+                        }
+                        listBox.Items.RemoveAt(i);
+                        textBox4.Text = (Convert.ToInt32(textBox4.Text) + 1).ToString();
+                                                
+                    }
+                }
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int eded_rnd2 = rnd.Next(1, 15);
+
+            random_eded_qutusu1.Text = (eded_rnd2).ToString();
+            if (textBox4.Text == "0")
+            {
+                textBox4.Text = "0";
+                MessageBox.Show("Artiq Yer Yoxdur!");
+            }
+            else
+            {
+                if (listBox.Items.Count < 10)
+                {
+                    
+
+                    if (!(list.Contains(eded_rnd2)))
+                    {
+
+                        listBox.Items.Add(eded_rnd2 + "         " + DateTime.Now.ToString());
+                        list.Add(eded_rnd2);
+                        textBox4.Text = (Convert.ToInt32(textBox4.Text) - 1).ToString();
+                    }
+                }
+                
+            }
+
+            
+
+            
+
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
